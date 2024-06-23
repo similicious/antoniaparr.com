ae05899ea00527bf2e1f29e1ee934f21fec9e930
similicious <mail@similicious.de>
2024-06-23 14:32:23 +0200

    feat: basic project detail page

---
+++ src/content/config.ts
@@ -0,0 +1,37 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// 2. Define your collection(s)
+const projectsCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      client: z.string().optional(),
+      date: z.date(),
+      description: z.string().optional(),
+      category: z.string(),
+      links: z
+        .array(
+          z.object({
+            linkAndTitle: z.object({ link: z.string(), linkTitle: z.string() }),
+          }),
+        )
+        .optional(),
+      icon: image(),
+      gallery: z
+        .array(
+          z.object({
+            imageAndDescription: z.object({
+              image: image(),
+              description: z.string(),
+            }),
+          }),
+        )
+        .optional(),
+    }),
+});
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  projects: projectsCollection,
+};
