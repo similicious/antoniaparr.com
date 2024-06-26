789ca5fa5f3f489718f5d23a374fb4b8607bd4e9
similicious <mail@similicious.de>
2024-06-26 12:24:36 +0200

    build: add purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import purgecss from "astro-purgecss";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [purgecss()],
+});
