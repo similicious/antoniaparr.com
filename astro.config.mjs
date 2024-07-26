1c593d4644f5e0ecd49b25de4254ef16b494cede
similicious <mail@similicious.de>
2024-07-26 09:30:50 +0200

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
-
 import purgecss from "astro-purgecss";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [purgecss()],
+  site: "https://antoniaparr.com",
+  integrations: [purgecss(), sitemap()],
 });
